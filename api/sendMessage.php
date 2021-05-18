6e07198c5e2fe25a0892057dcee2685a98ce799f
Sylvie-B <unsolo@hotmail.fr>
2021-05-18 11:09:55 +0200

    send message

--- api/sendMessage.php
+++ api/sendMessage.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Classes/Dialog.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Manager/dialogMana.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/DbChat.php';
 
